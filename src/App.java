19b6094518e04a46e7e90d07bf835923bf58db0d
kylepaz20 <lordseankyle0620@icloud.com>
2026-04-14 10:09:04 -0700

    Added four new classes for the home page

--- src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public void start() {
-        MainFrame frame = new MainFrame();
-        frame.setVisible(true);
-    }
-    
-}
--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new App().start();
-        });
-    }
-}
