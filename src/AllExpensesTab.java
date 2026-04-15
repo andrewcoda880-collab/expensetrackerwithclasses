223868242d180bb29df3efc21b26df4c910c9990
Andrew <andrew.coda.880@my.csun.edu>
2026-04-14 18:39:01 -0700

    more refactoring + branch creation

--- src/AllExpensesTab.java
+++ src/AllExpensesTab.java
@@ -4,7 +4,7 @@ public class AllExpensesTab extends JPanel {
 
     public AllExpensesTab() {
         JPanel allExpensesPanel = new JPanel();
-        
+        allExpensesPanel.setBackground(Constants.APP_COLOR);
         return;
     }
 }
