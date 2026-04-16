294cd72301dbb0db928429996764914be08e7921
donut <steve.branstad.883@my.csun.edu>
2026-04-16 12:16:35 -0700

    changes to the SettingsTab testing color changes

--- src/SettingsTab.java
+++ src/SettingsTab.java
@@ -1,8 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class SettingsTab extends JPanel {
-    
+
+        private JComboBox<String> settingOptions;
+        private JButton themeChange;
+
     public SettingsTab(){
         setLayout(new BorderLayout());
         setBackground(Constants.APP_COLOR);
@@ -10,12 +14,17 @@ public class SettingsTab extends JPanel {
         JLabel title = new JLabel("Setting$");
         title.setFont(new Font("Arial", Font.BOLD, 24));
 
-        add(title, BorderLayout.CENTER);
+        add(title, BorderLayout.NORTH);
+
+        String titleNotificationSettings = "Notification Settings";
+        notifsMenu = new Defau
+
 
         //hey!
         //yo
         //commit test
         //heyo
+        //starting at 10:58 I (steve) am now gona work on the settingstab
         
         
          
