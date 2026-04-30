c1899d1e418fb2283ffe7c3d948d02edadf8478d
Andrew <andrew.coda.880@my.csun.edu>
2026-04-30 11:18:44 -0700

    uhh

--- src/NavigationBar.java
+++ src/NavigationBar.java
@@ -3,25 +3,25 @@ import javax.swing.*;
 
 public class NavigationBar extends JPanel {
     public NavigationBar(CardLayout layout, JPanel container) {
-        JButton loginTabButton = new JButton ("Login");
+        JButton loginTabButton = new JButton("Login");
         JButton homeTabButton = new JButton("Home");
-        JButton expenseTabButton = new JButton ("Expenses");
-        JButton settingsTabButton = new JButton ("Settings");
-        JButton graphsTabButton = new JButton ("Graphs");
-        
+        JButton expenseTabButton = new JButton("Expenses");
+        JButton incomeTabButton = new JButton("Income");
+        JButton settingsTabButton = new JButton("Settings");
+        JButton graphsTabButton = new JButton("Graphs");
         
         loginTabButton.addActionListener(e -> layout.show(container, "LOGIN"));
-        homeTabButton. addActionListener(e -> layout.show(container, "HOME"));
+        homeTabButton.addActionListener(e -> layout.show(container, "HOME"));
         expenseTabButton.addActionListener(e -> layout.show(container, "EXPENSES"));
+        incomeTabButton.addActionListener(e -> layout.show(container, "MYINCOME"));
         graphsTabButton.addActionListener(e -> layout.show(container, "GRAPHS"));
         settingsTabButton.addActionListener(e -> layout.show(container, "SETTINGS"));
         
-
         add(loginTabButton);
         add(homeTabButton);
         add(expenseTabButton);
+        add(incomeTabButton);
         add(graphsTabButton);
         add(settingsTabButton);
-        
     }
 }
